6342738990893a001da34e29d49847bb5bef52d8
ahmed khairey <ahmed_khairey@yahoo.com>
2023-02-06 18:17:37 +0200

    resources create

--- resources/js/Pages/Projects/Create.vue
+++ resources/js/Pages/Projects/Create.vue
@@ -1,18 +1,14 @@
 <template lang="">
-    <Head title="Projects" />
+    <Head title="Create Project" />
 
 <AuthenticatedLayout>
     <template #header>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Projects</h2>
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">Create Project</h2>
     </template>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-end m=2 p-2">
-                <Link :href="route('projects.create')" class="px-4 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md" >
-                    New Project
-                </Link>
-            </div>
+            
         </div>
     </div>  
 </AuthenticatedLayout>
--- resources/js/Pages/Skills/index.vue
+++ resources/js/Pages/Skills/index.vue
@@ -8,6 +8,11 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="flex justify-end m=2 p-2">
+                <Link :href="route('skills.create')" class="px-4 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md" >
+                    New Skill
+                </Link>
+            </div>
         </div>
     </div>  
 </AuthenticatedLayout>
@@ -16,5 +21,5 @@
 
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 </script>
